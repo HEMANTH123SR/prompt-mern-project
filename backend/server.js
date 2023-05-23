e62936879a791bd8c562d75707702b0866366be3
hemanth <applicationcomputer257@gmail.com>
2023-05-23 12:41:01 +0530

    backend 1

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,42 @@
-console.log("hello world")
+const express=require('express');
+
+const port=5000;
+
+const app=express();
+
+app.use(express.json())
+
+app.get('/',(req,res)=>{
+res.status(200).json({
+    status:"suucces"
+})
+})
+
+app.post('/',(req,res)=>{
+res.status(200).json({
+  status: "suucces",
+});
+})
+
+app.get('/:id',(req,res)=>{
+res.status(200).json({
+  status: "suucces",
+});
+})
+
+app.patch('/:id',(req,res)=>{
+res.status(200).json({
+  status: "suucces",
+});
+})
+
+app.delete('/:id',(req,res)=>{
+res.status(200).json({
+  status: "suucces",
+});
+})
+
+
+app.listen(port,()=>{
+    console.log("server started listening reuest at port 5000")
+})
