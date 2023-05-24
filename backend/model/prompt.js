20f77e961edbd27602ece0f4927bda4672ffdd0f
hemanth <applicationcomputer257@gmail.com>
2023-05-24 20:35:04 +0530

    front end nav

--- backend/model/prompt.js
+++ backend/model/prompt.js
@@ -1,30 +1,24 @@
-const mongoose=require('mongoose')
+const mongoose = require("mongoose");
 
+const Schema = mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  title: {
+    type: String,
+    required: true,
+  },
+  prompt: {
+    type: String,
+    required: true,
+  },
+  rating: {
+    type: Number,
+  },
+  additionalUrl: {
+    type: String,
+  },
+});
 
-const Schema=mongoose.Schema(
-    {
-        name:{
-            type:String,
-            required:true
-        }
-        ,
-       title:{
-        type:String,
-        required:true
-       }
-       ,
-       prompt:{
-        type:String,
-        required:true
-       },
-       rating:{
-  type:Number
-       }
-       ,
-       additionalUrl:{
-        type:String
-       }
-    }
-)
-
-module.exports=mongoose.model('promptDB',Schema)
+module.exports = mongoose.model("promptDB", Schema);
---
+++ frontend/src/components/Navbar.js
@@ -0,0 +1,26 @@
+const Navbar = () => {
+  return (
+    <nav className="">
+      <div>
+        <h1>
+          Prom<span>oter</span>
+        </h1>
+      </div>
+      <div>
+        <ul>
+          <li>
+            <a href="">Prompt</a>
+          </li>
+          <li>
+            <a href="">Blog</a>
+          </li>
+          <li>
+            <a href="">👀</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
